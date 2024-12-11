35aa4e2eebf60aa5102f5bc17ee5bfc8656cd08a
Sofa <sofasven00@gmail.com>
2024-12-11 14:43:01 +0300

    Pods added

---
+++ FrankensteinApp/FrankensteinApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrankensteinApp
+//
+//  Created by Софа on 11.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
